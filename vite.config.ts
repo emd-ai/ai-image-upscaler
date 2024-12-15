25bc6f2a0b13688dc1c90b3cdad8c76284799291
emd-ai <contact@aihaven.com>
2024-12-15 20:42:38 +0700

    fix: update vite config to properly handle env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': {}
-  },
-  server: {
-    hmr: {
-      overlay: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': env
     },
-    watch: {
-      usePolling: true,
+    server: {
+      hmr: {
+        overlay: true,
+      },
+      watch: {
+        usePolling: true,
+      },
     },
-  },
+  }
 })
