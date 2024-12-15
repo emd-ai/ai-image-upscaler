97c3d8f3aeff1c3b74a3e0bea86c5030dc25d828
emd-ai <contact@aihaven.com>
2024-12-15 20:09:01 +0700

    fix: update tailwind config to use ESM imports

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import aspectRatio from '@tailwindcss/aspect-ratio'
+
 export default {
   content: [
     "./index.html",
@@ -8,6 +10,6 @@ export default {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/aspect-ratio'),
+    aspectRatio,
   ],
 }
